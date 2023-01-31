9039ae949cb18b26a8779296471439745d08585b
MUC00 <91524150+MUC00@users.noreply.github.com>
2023-01-31 10:38:46 +0100

    try to fix it v2

--- team-green/src/recos.ts
+++ team-green/src/recos.ts
@@ -2,43 +2,43 @@ export const allRecommendations = {
   bugatti: [
     {
       id: '3',
-      image: require('./images/reco_1.jpg'),
+      image: require('./images/black_yacht.png'),
     },
     {
       id: '5',
-      image: require('./images/reco_2.jpg'),
+      image: require('./images/black_mansion.png'),
     },
     {
       id: '6',
-      image: require('./images/reco_3.jpg'),
+      image: require('./images/black_jet.png'),
     },
   ],
   rolls: [
     {
       id: '3',
-      image: require('./images/reco_4.jpg'),
+      image: require('./images/golden_yacht.png'),
     },
     {
       id: '6',
-      image: require('./images/reco_5.jpg'),
+      image: require('./images/golden_mansion.png'),
     },
     {
       id: '4',
-      image: require('./images/reco_6.jpg'),
+      image: require('./images/golden_jet.png'),
     },
   ],
   ferrari: [
     {
       id: '1',
-      image: require('./images/reco_7.jpg'),
+      image: require('./images/red_yacht.png'),
     },
     {
       id: '8',
-      image: require('./images/reco_8.jpg'),
+      image: require('./images/red_mansion.png'),
     },
     {
       id: '7',
-      image: require('./images/reco_9.jpg'),
+      image: require('./images/red_jet.png'),
     },
   ],
 };
--- team-red/src/product.tsx
+++ team-red/src/product.tsx
@@ -1,5 +1,5 @@
 export const product = {
-  name: 'Car',
+  name: '',
   variants: [
     {
       sku: 'bugatti',
